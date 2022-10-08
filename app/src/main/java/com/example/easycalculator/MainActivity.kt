847d98ecfa2866ddb477fa7c781f8b0f7401f91a
keyoz11 <70904451+keyoz11@users.noreply.github.com>
2022-10-08 12:28:25 +0300

    first commit,added functions buttons inputs layouts

---
+++ app/src/main/java/com/example/easycalculator/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.easycalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun divide(view: View) {
+
+
+    }
+    fun plus(view: View) {
+
+
+    }
+    fun minus(view: View) {
+
+
+    }
+    fun multiply(view: View) {
+
+        
+
+    }
+
+}
